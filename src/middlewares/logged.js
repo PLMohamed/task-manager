7460316de8a06456fb27f2a03c73d6c997008bf7
PLMohamed <141277277+PLMohamed@users.noreply.github.com>
2024-02-07 20:44:35 +0100

    Fixed Middleware

--- src/middlewares/logged.js
+++ src/middlewares/logged.js
@@ -1,8 +1,6 @@
 import { verifyJwtToken } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
-const { JWT_SECRET_KEY } = process.env;
-
 /**
  * @param {NextRequest} request
  * @returns {Promise<NextResponse>}
